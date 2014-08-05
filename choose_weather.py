2fb2367018707a0e44bbb0b2a06e17e7895a9f40
KaiyoteSoft <Spikeyhedgehog141@gmail.com>
2014-08-05 10:56:33 -0700

    Added font switching, fixed graphics, added detailed surf report, problems still exist

--- choose_weather.py
+++ choose_weather.py
@@ -3,17 +3,41 @@ import urllib2
 import datetime
 import json
 import pprint
+import random
 
 class Weather():
-    def __init__(self, city):
+    def __init__(self, city, font):
         import time
         self.city = city
+        self.font = font
         self.internet_connection()
+        self.spot = 147
 
-        weather_font = pygame.font.Font("fonts/animeace2_reg.ttf", 35)
-        weather_font_large = pygame.font.Font("fonts/animeace2_reg.ttf", 45)
-        wind_font = pygame.font.Font("fonts/animeace2_reg.ttf", 28)
-        title_font = pygame.font.Font("fonts/ASTONISH.TTF", 200)
+        if self.font == "random":
+            RED = ((255, 0, 0))
+            GREEN = ((0, 255, 0))
+            LIGHT_BLUE = ((85, 190, 255))
+            LIGHT_GREEN = ((32, 255, 128))
+            PURPLE = ((236, 32, 255))
+            PINK = ((255, 32, 206))
+            WHITE = ((255, 255, 255))
+            normal_font = random.choice(["Gabrielle.ttf", "animeace2_reg.ttf", "SketchRockwell-Bold.ttf"])
+            weather_font = pygame.font.Font("fonts/{}".format(normal_font), 35)
+            weather_font_large = pygame.font.Font("fonts/{}".format(normal_font), 45)
+            wind_font = pygame.font.Font("fonts/{}".format(normal_font), 28)
+
+
+
+        if self.font == "normal":
+            normal_font = ("animeace2_reg.ttf")
+            weather_font = pygame.font.Font("fonts/{}".format(normal_font), 35)
+            weather_font_large = pygame.font.Font("fonts/{}".format(normal_font), 45)
+            wind_font = pygame.font.Font("fonts/{}".format(normal_font), 28)
+            #title_font = pygame.font.Font("fonts/{}".format(normal_font), 200)
+
+        self.middle_surf = pygame.image.load("img/middle_surf.png")
+        self.small_surf = pygame.image.load("img/tahoe.jpg")
+        self.large_surf = pygame.image.load("img/big_wave.jpg")
 
         RED = ((255, 0, 0))
         GREEN = ((0, 255, 0))
@@ -27,6 +51,17 @@ class Weather():
             data = self.info.read()
             j_data = json.loads(data)
 
+            surf_data = self.surf.read()
+            j_surf_data = json.loads(surf_data)
+            # print(j_surf_data)
+
+            surf_data_2 = self.surf_2.read()
+            j_surf_data_2 = json.loads(surf_data_2)
+
+            tide_data = self.tide.read()
+            j_tide_data = json.loads(tide_data)
+
+#### loading statements for weather
             max_temp = (j_data["main"]["temp_max"])
             min_temp = (j_data["main"]["temp_min"])
             current_temp = (j_data["main"]["temp"])
@@ -61,6 +96,121 @@ class Weather():
             self.menu_rect = pygame.Rect(1040, 575, 40, 40)
             self.sunrise_rect = pygame.Rect(50, 410, 600, 100)
 
+    ### loading statements for surf
+            if self.city == ("Surf, ca"):
+                wave_condition = (j_surf_data[0]["shape_detail"]["swell"])
+                self.wave_height = (j_surf_data[6]["size_ft"])
+                location = (j_surf_data[0]["spot_name"])
+
+                self.height = wind_font.render("The location is {} and the waves are {} feet".format(location, ("%.2f" % self.wave_height)),
+                                               True, RED)
+                self.condition = weather_font.render("The surf is {}".format(wave_condition), True, WHITE)
+
+            #### 2nd surf report
+                wave_condition_2 = (j_surf_data_2[0]["shape_detail"]["swell"])
+                wave_height_2 = (j_surf_data_2[6]["size_ft"])
+                location_2 = (j_surf_data_2[0]["spot_name"])
+
+                self.height_2 = wind_font.render("The location is {} and the waves are {} feet".format(location_2, ("%.2f" % wave_height_2)),
+                                               True, GREEN)
+                self.condition_2 = weather_font.render("The surf is {}".format(wave_condition_2), True, WHITE)
+
+            ### tide
+                self.morning_tide = (j_tide_data[5]["tide"])
+                self.afternoon_tide = (j_tide_data[14]["tide"])
+                self.evening_tide = (j_tide_data[18]["tide"])
+                self.morning_tide_text = weather_font.render("The tide at 5 in the morning is {}".format
+                                                             (("%.2f" % self.morning_tide)), True, LIGHT_BLUE)
+                self.afternoon_tide_text = weather_font.render("The tide at 2 in the afternoon is {}".format
+                                                               (("%.2f" % self.afternoon_tide)), True, LIGHT_BLUE)
+                self.evening_tide_text = weather_font.render("The tide at 6 in the evening is {}".format
+                                                             (("%.2f" % self.evening_tide)), True, LIGHT_BLUE)
+
+
+
+            ### rectangles for surf
+                self.height_rect = pygame.Rect(20, 20, 600, 100)
+                self.condition_rect = pygame.Rect(500, 120, 600, 100)
+
+                self.surf_height_rect_2 = pygame.Rect(10, 80, 600, 100)
+                self.morning_tide_rect = pygame.Rect(200, 160, 600, 100)
+                self.afternoon_tide_rect = pygame.Rect(200, 200, 600, 100)
+                self.evening_tide_rect = pygame.Rect(200, 240, 600, 100)
+
+                self.middle_surf_rect = pygame.Rect(300, 465, 350, 350)
+
+                print(self.height)
+
+###### IF THE FONT = RANDOM THEN U HAVE
+            ###### TO REDRAW EVERYTHING..
+### UNFORTUNATELY, IT IS NOT WORKING EVERY TIME???
+        if self.font == "random":
+            RED = ((255, 0, 0))
+            GREEN = ((0, 255, 0))
+            LIGHT_BLUE = ((85, 190, 255))
+            LIGHT_GREEN = ((32, 255, 128))
+            PURPLE = ((236, 32, 255))
+            PINK = ((255, 32, 206))
+            WHITE = ((255, 255, 255))
+            normal_font = random.choice(["Gabrielle.ttf", "animeace2_reg.ttf", "SketchRockwell-Bold.ttf"])
+            weather_font = pygame.font.Font("fonts/{}".format(normal_font), 35)
+            weather_font_large = pygame.font.Font("fonts/{}".format(normal_font), 45)
+            wind_font = pygame.font.Font("fonts/{}".format(normal_font), 28)
+            title_font = pygame.font.Font("fonts/{}".format(normal_font), 200)
+            print("Change font")
+
+            self.description = weather_font_large.render("The name of the city is " + j_data["name"], True, RED)
+            self.description_2 = wind_font.render("It is located in the country of " + j_data["sys"]["country"], True, RED)
+            self.weather = weather_font.render(j_data["weather"][0]["description"], True, GREEN)
+            self.maximum_temp = weather_font.render("The maximum temperature today is {} degrees".format(max_temp), True, PURPLE)
+            self.minimum_temp = weather_font.render("The minimum temperature will be {} degrees".format(min_temp), True, PURPLE)
+            self.current_temperature = weather_font.render("The current temperature is {} degrees.".format(current_temp), True, LIGHT_BLUE)
+            self.wind_description = wind_font.render("There is a wind blowing at {} mph ".format(wind_speed), True, LIGHT_GREEN)
+            self.day = weather_font.render("The date is {}".format(current_time), True, PINK)
+            self.sunrise_text = weather_font.render("The sun rises at {} and sets at {}".format(sunrise_time, sunset_time), True, LIGHT_BLUE)
+
+            self.height_rect = pygame.Rect(20, 20, 600, 100)
+            self.condition_rect = pygame.Rect(500, 120, 600, 100)
+
+            self.surf_height_rect_2 = pygame.Rect(10, 80, 600, 100)
+            self.morning_tide_rect = pygame.Rect(200, 160, 600, 100)
+            self.afternoon_tide_rect = pygame.Rect(200, 200, 600, 100)
+            self.evening_tide_rect = pygame.Rect(200, 240, 600, 100)
+
+            self.middle_surf_rect = pygame.Rect(300, 465, 350, 350)
+
+            # if self.city == ("Surf, ca"):
+            wave_condition = (j_surf_data[0]["shape_detail"]["swell"])
+            self.wave_height = (j_surf_data[6]["size_ft"])
+            location = (j_surf_data[0]["spot_name"])
+
+            self.height = wind_font.render("The location is {} and the waves are {} feet".format(location, ("%.2f" % self.wave_height)),
+                                           True, RED)
+            self.condition = weather_font.render("The surf is {}".format(wave_condition), True, WHITE)
+
+        #### 2nd surf report
+            wave_condition_2 = (j_surf_data_2[0]["shape_detail"]["swell"])
+            wave_height_2 = (j_surf_data_2[6]["size_ft"])
+            location_2 = (j_surf_data_2[0]["spot_name"])
+
+            self.height_2 = wind_font.render("The location is {} and the waves are {} feet".format(location_2, ("%.2f" % wave_height_2)),
+                                           True, GREEN)
+            self.condition_2 = weather_font.render("The surf is {}".format(wave_condition_2), True, WHITE)
+
+        ### tide
+            self.morning_tide = (j_tide_data[5]["tide"])
+            self.afternoon_tide = (j_tide_data[14]["tide"])
+            self.evening_tide = (j_tide_data[18]["tide"])
+            self.morning_tide_text = weather_font.render("The tide at 5 in the morning is {}".format
+                                                         (("%.2f" % self.morning_tide)), True, LIGHT_BLUE)
+            self.afternoon_tide_text = weather_font.render("The tide at 2 in the afternoon is {}".format
+                                                           (("%.2f" % self.afternoon_tide)), True, LIGHT_BLUE)
+            self.evening_tide_text = weather_font.render("The tide at 6 in the evening is {}".format
+                                                         (("%.2f" % self.evening_tide)), True, LIGHT_BLUE)
+
+
+            pygame.display.update()
+
     #def choose_weather(self, city):
     def draw(self):
         main_surface = pygame.Surface((1280, 770))
@@ -75,12 +225,43 @@ class Weather():
         #main_surface.blit(self.palo_alto_pic, self.picture_rect)
         main_surface.blit(self.sunrise_text, self.sunrise_rect)
 
+        if self.font == random:
+            pygame.display.update()
+
+
         return(main_surface)
+
+    def surf_draw(self):
+        main_surface = pygame.Surface((1280, 770))
+        main_surface.blit(self.height, self.height_rect)
+        main_surface.blit(self.condition, self.condition_rect)
+        main_surface.blit(self.height_2, self.surf_height_rect_2)
+
+        main_surface.blit(self.morning_tide_text, self.morning_tide_rect)
+        main_surface.blit(self.evening_tide_text, self.evening_tide_rect)
+        main_surface.blit(self.afternoon_tide_text, self.afternoon_tide_rect)
+
+        # if self.font == random:
+        #     pygame.display.update()
+
+        ### if statement for surf pictures
+        if self.wave_height < 2:
+            main_surface.blit(self.small_surf, self.middle_surf_rect)
+        if self.wave_height > 2 and self.wave_height < 5:
+            main_surface.blit(self.middle_surf, self.middle_surf_rect)
+        if self.wave_height > 5:
+            main_surface.blit(self.large_surf, self.middle_surf_rect)
+
+        return(main_surface)
+
 
     def internet_connection(self):
         self.connected = True
         try:
             self.info = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial'.format(self.city))
-
+            # self.surf = urllib2.urlopen('http://api.spitcast.com/api/spot/forecast/{}/'.format(self.spot))
+            self.surf = urllib2.urlopen('http://api.spitcast.com/api/spot/forecast/147/')
+            self.surf_2 = urllib2.urlopen('http://api.spitcast.com/api/spot/forecast/4/')
+            self.tide = urllib2.urlopen('http://api.spitcast.com/api/county/tide/santa-cruz/')
         except IOError:
             self.connected = False
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from choose_weather import Weather
 import pprint
 import time
 import datetime
+import random
 
 try:
     import android
@@ -36,17 +37,25 @@ LIGHT_GREEN = ((32, 255, 128))
 PURPLE = ((236, 32, 255))
 PINK = ((255, 32, 206))
 WHITE = ((255, 255, 255))
+BLUE = (0, 0, 255)
 
-weather_font = pygame.font.Font("fonts/animeace2_reg.ttf", 35)
-weather_font_large = pygame.font.Font("fonts/animeace2_reg.ttf", 45)
-wind_font = pygame.font.Font("fonts/animeace2_reg.ttf", 28)
+
+
+normal_font = ("animeace2_reg.ttf")
+random_font = random.choice(["Gabrielle.ttf", "animeace2_reg.ttf", "SketchRockwell-Bold.ttf"])
+
+
+weather_font = pygame.font.Font("fonts/{}".format(normal_font), 35)
+weather_font_large = pygame.font.Font("fonts/{}".format(normal_font), 45)
+wind_font = pygame.font.Font("fonts/{}".format(normal_font), 28)
 title_font = pygame.font.Font("fonts/ASTONISH.TTF", 200)
 sorry_font = pygame.font.Font("fonts/ASTONISH.TTF", 150)
 
 
 palo_alto_pic = pygame.image.load("img/palo_alto.jpg")
-rain_pic = pygame.image.load("img/rain.jpg")
-sun_pic = pygame.image.load("img/sun.jpg")
+capitola_pic = pygame.image.load("img/capitola.jpg")
+rain_pic = pygame.image.load("img/rain.png")
+sun_pic = pygame.image.load("img/sun.png")
 lightning_pic = pygame.image.load("img/lightning.jpg")
 
 
@@ -58,21 +67,32 @@ if len(pull) < 2:
 else:
     city = (pull[1])
 
-weather = Weather("Palo Alto, ca")
+weather = Weather("Palo Alto, ca", font = "normal")
 ### checking for internet connection
 
 if weather.connected == True:
 
     pa_button = True
     sc_button = True
+    surf_rep = True
+    setting_menu = True
 
     pa_1 = pygame.Rect(90, 120, 40, 40)
     pa_2 = pygame.Rect(90, 180, 40, 40)
     sc_rect = pygame.Rect(70, 450, 40, 40)
     pa_weather = pygame.draw.circle(windowSurface, RED, (150, 175), 100, 5)
     sc_weather = pygame.draw.circle(windowSurface, LIGHT_BLUE, (150, 475), 100, 5)
-    menu = pygame.draw.circle(windowSurface, WHITE, (1100, 600), 100, 5)
+    surf_circle = pygame.draw.circle(windowSurface, BLUE, (425, 175), 100, 5)
 
+### setting fonts and rects for settings
+    setting_circle_font = pygame.draw.circle(windowSurface, LIGHT_BLUE, (150, 150), 100, 5)
+    setting_font_rect = pygame.Rect(455, 130, 100, 100)
+    setting_rect = pygame.Rect(1040, 205, 40, 40)
+
+
+    menu = pygame.draw.circle(windowSurface, WHITE, (1100, 600), 100, 5)
+    surf_rect1 = pygame.Rect(365, 120, 40, 40)
+    surf_rect2 = pygame.Rect(345, 180, 40, 40)
 
     title_rect = pygame.Rect(580, 10, 600, 100)
     title_2_rect = pygame.Rect(640, 80, 600, 100)
@@ -89,6 +109,13 @@ if weather.connected == True:
     pa_2_text = weather_font.render("Alto", True, WHITE)
     sc_text = wind_font.render("Capitola", True, LIGHT_GREEN)
     menu_text = weather_font.render("Home", True, LIGHT_BLUE)
+    surf_text = weather_font.render("Surf", True, GREEN)
+    surf_text_2 = weather_font.render("Report", True, GREEN)
+
+### setting circles
+    setting_text = wind_font.render("Settings", True, WHITE)
+    setting_font = weather_font.render("Font", True, LIGHT_BLUE)
+    setting_circle = pygame.draw.circle(windowSurface, WHITE, (825, 175), 100, 5)
 
 else:
     print("Sorry, no internet connection. Try again later")
@@ -108,21 +135,62 @@ while True:
             sys.exit()
         mouse_pos = pygame.mouse.get_pos()
         if event.type == MOUSEBUTTONDOWN:
-            if pa_weather.collidepoint(mouse_pos):
+            if pa_weather.collidepoint(mouse_pos) and surf_rep == True \
+                    and sc_button == True:
                 pa_button = False
-                weather = Weather(city = "Palo Alto, ca")
+                weather = Weather(city = "Palo Alto, ca", font = "normal")
                 # pa_button_2 = False
-            if sc_weather.collidepoint(mouse_pos):
+
+            if sc_weather.collidepoint(mouse_pos) and surf_rep == True\
+                    and pa_button == True:
                 sc_button = False
-                weather = Weather(city = "Capitola, ca")
+                weather = Weather(city = "Capitola, ca", font = "normal")
+
             if menu.collidepoint(mouse_pos):
                 pa_button = True
                 sc_button = True
+                surf_rep = True
+                setting_menu = True
+            if surf_circle.collidepoint(mouse_pos) and pa_button == True\
+                    and sc_button ==  True:
+                surf_rep = False
+                weather = Weather(city = "Surf, ca", font = "normal")
+
+    ### settings circles
+            if setting_circle.collidepoint(mouse_pos) and pa_button == True and sc_button == True\
+                    and surf_rep == True:
+                setting_menu = False
+            if setting_circle_font.collidepoint(mouse_pos):
+                #print("CHANGE THE FONT")
+                normal_font = random.choice(["Gabrielle.ttf", "animeace2_reg.ttf", "SketchRockwell-Bold.ttf"])
+                weather_font = pygame.font.Font("fonts/{}".format(normal_font), 35)
+                weather_font_large = pygame.font.Font("fonts/{}".format(normal_font), 45)
+                wind_font = pygame.font.Font("fonts/{}".format(normal_font), 28)
+                ### setting fonts and rects for settings
+                setting_circle_font = pygame.draw.circle(windowSurface, LIGHT_BLUE, (150, 150), 100, 5)
+
+                title = title_font.render("Weather", True, PURPLE)
+                title_2 = title_font.render("App", True, LIGHT_BLUE)
+                pa_1_text = weather_font.render("Palo", True, WHITE)
+                pa_2_text = weather_font.render("Alto", True, WHITE)
+                sc_text = wind_font.render("Capitola", True, LIGHT_GREEN)
+                menu_text = weather_font.render("Home", True, LIGHT_BLUE)
+                surf_text = weather_font.render("Surf", True, GREEN)
+                surf_text_2 = weather_font.render("Report", True, GREEN)
+
+            ### setting circles
+                setting_text = wind_font.render("Settings", True, WHITE)
+                setting_font = weather_font.render("Font", True, LIGHT_BLUE)
+                setting_circle = pygame.draw.circle(windowSurface, WHITE, (825, 175), 100, 5)
+
+                weather = Weather(city, font = "random")
+                pygame.display.update()
+
+
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-
 
     if weather.connected == True:
 
@@ -138,12 +206,19 @@ while True:
             windowSurface.blit(lightning_pic, lightning_rect)
             windowSurface.blit(sun_pic, sun_rect)
             windowSurface.blit(rain_pic, rain_rect)
+        ### surf report
+            surf_circle = pygame.draw.circle(windowSurface, BLUE, (425, 175), 100, 5)
+            windowSurface.blit(surf_text, surf_rect1)
+            windowSurface.blit(surf_text_2, surf_rect2)
+        ### settings circle
+            setting_circle = pygame.draw.circle(windowSurface, WHITE, (1125, 235), 100, 5)
+            windowSurface.blit(setting_text, setting_rect)
+
+
+
 
         if pa_button == False:
             windowSurface.fill((0,0,0))
-            # windowSurface.blit(choose_weather, description_rect)
-            #weather.draw()
-            # windowSurface.blit(sunrise_text, sunrise_rect)
             windowSurface.blit(weather.draw(), (0, 0))
             windowSurface.blit(palo_alto_pic, picture_rect)
             menu = pygame.draw.circle(windowSurface, WHITE, (1100, 600), 100, 5)
@@ -153,10 +228,22 @@ while True:
             windowSurface.fill((0,0,0))
             windowSurface.blit(weather.draw(), (0, 0))
             menu = pygame.draw.circle(windowSurface, WHITE, (1100, 600), 100, 5)
+            windowSurface.blit(capitola_pic, picture_rect)
             windowSurface.blit(menu_text, menu_rect)
 
+        if surf_rep == False:
+            windowSurface.fill((0,0,0))
+            windowSurface.blit(weather.surf_draw(), (0, 0))
+            menu = pygame.draw.circle(windowSurface, WHITE, (1100, 600), 100, 5)
+            windowSurface.blit(menu_text, menu_rect)
 
-
+        if setting_menu == False:
+            windowSurface.fill((0,0,0))
+            menu = pygame.draw.circle(windowSurface, WHITE, (1100, 600), 100, 5)
+            windowSurface.blit(menu_text, menu_rect)
+            setting_circle_font = pygame.draw.circle(windowSurface, LIGHT_BLUE, (500, 150), 100, 5)
+            windowSurface.blit(setting_font, setting_font_rect)
+            #### fonts
 
 
     if weather.connected == False:
